00ebae303f98a92ae1e5f884e0227c698ca34506
ericjen01 <ericjen01>
2023-11-15 03:45:16 -0800

    exercise 1.3 completed

--- part1/course_information/src/App.jsx
+++ part1/course_information/src/App.jsx
@@ -1,36 +1,29 @@
 /* eslint-disable react/prop-types */
 const App = () => {
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const Header = (props) => (
-<div>
-      <h1>{props.crs}</h1> 
-    </div>   
-  )
+  const Header = (props) => ( <div> <h1>{props.crs}</h1> </div> )
 
-  const Total = (props) => (
-    <div>
-      <p>Total Score: {props.tt}</p>
-    </div>
-  )
-
-  const Content1 = (props) => (
-    <div>
-      <p>{props.pt} {props.exe}</p> 
-    </div>
-  )
+  const Total = (props) => ( <div> <p>Total Score: {props.tt}</p> </div>)
 
   const Content2 = () => (
     <div>
-      <Part pt={part1} exe={exercises1} />
-      <Part pt={part2} exe={exercises2} />
-      <Part pt={part3} exe={exercises3} />
+      <Part pt={part1.name} exe={part1.exercises} />
+      <Part pt={part2.name} exe={part2.exercises} />
+      <Part pt={part3.name} exe={part3.exercises} />
     </div>
   )
 
@@ -43,16 +36,8 @@ const App = () => {
   return (
     <div>
       <Header crs={course} />
-      <Content1 pt={part1} exe={exercises1}/>
-      <Content1 pt={part3} exe={exercises2}/>
-      <Content1 pt={part2} exe={exercises3}/>
-      <Total tt={exercises1 + exercises2 + exercises3}/>
-   
-      --------------
-
-      <Header crs={course} />
       <Content2 />
-      <Total tt={exercises1 + exercises2 + exercises3}/>
+      <Total tt={part1.exercises + part2.exercises + part3.exercises}/>
 
     </div>
   )
