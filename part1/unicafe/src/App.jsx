7877b6cb72fe06bb76857ea3e2c036e71ff95a11
ericjen01 <ericjen01>
2023-11-16 01:33:40 -0800

    exercise 1.11 unicafe bonus

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,20 +2,25 @@
 import { useState } from 'react'
 
 const Stats = (props) => (
-  <>
+  <table>
+    <tr>
+      <td>stats</td>
+      <td>outcome</td>
+    </tr>
   <StatsLine txt={"good: "} calc= {props.good}/>
   <StatsLine txt={"neutral: "} calc= {props.neutral}/>
   <StatsLine txt={"bad: "} calc= {props.bad}/>
   <StatsLine txt={"total: "} calc= {props.good + props.neutral + props.bad}/>
   <StatsLine txt={"average: "} calc= {(props.good*1 + props.neutral*0 + props.bad*-1)/(props.good+props.neutral+props.bad)}/>
   <StatsLine txt={"postive: "} calc= {(props.good)/(props.good+props.neutral+props.bad)*100 +"%"}/>
-  </>
+  </table>
 )
 
 const StatsLine = (props) => (
-  <div>
-    <span>{props.txt} {props.calc}</span>
-  </div>
+  <tr>
+    <td>{props.txt}</td>
+    <td>{props.calc}</td>
+  </tr>
 )
 
 const Button = (props) =>(
