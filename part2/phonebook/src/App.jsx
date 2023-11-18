ffb5e1ae66a81cf819b15ddb822300df5bdcbce3
ericjen01 <ericjen01>
2023-11-17 23:59:19 -0800

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,11 +3,16 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 
+const Alert = ({message}) =>(
+  message===null? (null) :  (<h4>{message}</h4>) 
+)
+
 const App = () => {
 
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
-
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [message, setMessage] = useState('')
 
   const duplicateName = persons.find(p => p.name === newName)
 
@@ -15,20 +20,29 @@ const App = () => {
     e.preventDefault();
 
     const person = {name:newName}
-    
-    duplicateName ? alert(`${newName} is already added to phonebook`) 
+  
+    duplicateName 
+    ? setMessage(`" ${newName} " is already added to phonebook`)
     : setPersons(persons.concat(person))
-      
-  }
 
+    setTimeout(() => {
+      setMessage(null)
+    }, 4000)
+  }
+  
   const handlePersonChange = (e) => setNewName(e.target.value)
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Alert message={message}>test</Alert>
       <form onSubmit={submitAddition}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
